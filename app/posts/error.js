4e5606225aff5d6c44056cb60462c9d3f26dbbe4
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-15 23:38:58 +0600

    explore next js routing system

---
+++ app/posts/error.js
@@ -0,0 +1,17 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+const Error = ({ error, reset }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+};
+
+export default Error;
---
+++ app/posts/new/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>new post</div>;
+};
+
+export default page;
